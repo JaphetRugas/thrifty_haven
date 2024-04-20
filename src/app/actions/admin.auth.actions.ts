5c17d5275ccd995ac16fd862ace97933faa0a5b1
Jade Japhet Rugas <rugas.jadejaphet.123@gmail.com>
2024-04-20 21:47:48 +0800

    feat: server action for user and admin signup

--- src/app/actions/admin.auth.actions.ts
+++ src/app/actions/admin.auth.actions.ts
@@ -1,73 +1,22 @@
 "use server"
 
 import { z } from "zod"
-import { signUpFormSchema, signInFormSchema } from "../../types"
-import { Argon2id } from "oslo/password"
-import { generateId } from "lucia"
-import { userTable } from "@/database/schema"
+import { signInFormSchema } from "../../types"
+import { Argon2id } from "oslo/password" 
 import db from "@/database"
 import { lucia, validateRequest } from "@/database/auth"
 import { cookies } from "next/headers"
 import { eq } from "drizzle-orm"
-import { error } from "console"
 
-
-export const signUp = async (values: z.infer<typeof signUpFormSchema>) => {
-    console.log(values)
-    const hashedPassword = await new Argon2id().hash(values.password)
-    const userId = generateId(15) 
-    const birthday = new Date(values.birthday);
-
-    try {
-        await db
-            .insert(userTable)
-            .values({
-                id: userId,
-                email: values.email,
-                firstName: values.firstName,
-                lastName: values.lastName,
-                age: values.age,
-                birthday: birthday,
-                phoneNumber: values.phoneNumber,
-                passwordHash: hashedPassword,
-                createdAt: new Date(),
-            })
-            .returning({
-                id: userTable.id,
-                email: userTable.email,
-            })
-
-        return {
-            success: true,
-            values: {
-                userId,
-            },
-        };
-
-    } catch (e) {
-        return {
-            success: false,
-            error: e,
-        };
-    }
-}
-
-export const signIn = async (values: z.infer<typeof signInFormSchema>) => {
+export const signIn = async (values: z.infer<typeof signInFormSchema>) => { 
     const existingUser = await db.query.userTable.findFirst({
         where: (table) => eq(table.email, values.email),
-    })
+    }) 
 
     if (!existingUser) {
         return {
             success: false,
             error: "User not found"
-        }
-    }
- 
-    if (existingUser.isAdmin) { 
-        return {
-            success: false,
-            error: "Admins cannot log in using this function"
         }
     }
 
