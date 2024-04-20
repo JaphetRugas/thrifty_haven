38759e50737724e9bd29ffe469a0d62281531e4f
Jade Japhet Rugas <rugas.jadejaphet.123@gmail.com>
2024-04-20 16:18:56 +0800

    feat: create sign in form

---
+++ src/app/private/page.tsx
@@ -0,0 +1,10 @@
+import PageComponent from '@/components/pageComponent'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div> 
+        <PageComponent />
+    </div>
+  )
+}
